3a677f8f213b119f1220c2108d72f75dc75fb962
Jon Hanson <jon.hanson@ave81.com>
2017-12-07 14:30:08 -0600

    smooth scroll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,8 @@
 //= require ./all_nosearch
 //= require ./app/_search
 
+//  Drip.com header
+
 !function(o, l, n) {
  "use strict";
  var c = {
@@ -17,3 +19,14 @@
      c.windowScroll()
  })
 }(window, window.jQuery);
+
+
+// smooth scroll
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
