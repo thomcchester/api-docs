c1a0be09c0446e3755b493d4091b564ad89b8ce8
Jon Hanson <jon.hanson@ave81.com>
2017-12-11 14:57:22 -0600

    remove unneeded js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,26 +1,6 @@
 //= require ./all_nosearch
 //= require ./app/_search
 
-//  Drip.com header
-
-!function(o, l, n) {
- "use strict";
- var c = {
-     mobileNavToggleClick: function(o) {
-         l("html").toggleClass("nav-open")
-     },
-     windowScroll: function(n) {
-         l(o).scrollTop() > 0 ? l("html").addClass("scrolled") : l("html").removeClass("scrolled")
-     }
- };
- l(document).ready(function() {
-     l(document).on("click", ".header-toggle-button", c.mobileNavToggleClick),
-     l(o).on("scroll", c.windowScroll),
-     c.windowScroll()
- })
-}(window, window.jQuery);
-
-
 // smooth scroll
 
 $(document).on('click', 'a[href^="#"]', function (event) {
